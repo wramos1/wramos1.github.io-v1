1ecd8112fcf297cd46c39616e4362c5d0ddfd8d2
Willyam Ramos <ramoswillyam1@gmail.com>
2022-05-30 18:58:43 -0700

    Complete Site Overhaul

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Loaded...')
-});
+//Variables Irrelevant To Functions
+
+
+//Functions & Methods
+
+function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 75;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    };
+};
+
+function lightUp() {
+    const navbar = document.querySelector('.navbar');
+
+    if (window.scrollY > 0) {
+        navbar.classList.add('underline')
+    } else {
+        navbar.classList.remove('underline')
+    }
+
+}
+
+//Calls and Inits
+
+window.addEventListener('scroll', reveal);
+
+window.addEventListener('scroll', lightUp);
+
